d4b3efe2ed06991fdb75d43afdca37741632d36f
Hastings Greer <hastings.greer@kitware.com>
2020-05-13 12:25:10 -0400

    dummy sender and reciever

---
+++ dummy_recieve.py
@@ -0,0 +1,43 @@
+import numpy as np
+import socket
+import cv2
+
+def process(x, metadata):
+    cv2.imshow(metadata, x)
+    cv2.waitKey(1)
+
+def get_connection():
+    s = socket.socket(socket.AF_INET,
+	              socket.SOCK_STREAM)
+
+    s.bind(("", 9999))
+
+    s.listen(1)
+    conn, addr = s.accept()
+    print("got connection")
+
+    lenMetadata = np.frombuffer(conn.recv(4), dtype=np.int32)[0]
+
+    metadata = str(conn.recv(lenMetadata), "utf-8")
+
+    width, height = np.frombuffer(conn.recv(8), dtype=np.int32)
+
+    print(width, height)
+    while True:
+        message_list = []
+        while sum([len(m) for m in message_list]) < width * height * 2:
+            message = conn.recv(width * height * 2)
+            if len(message) == 0:
+                print("connection died")
+                s.close()
+                return
+            message_list.append(message)
+        message = b"".join(message_list)
+
+        x = np.fromstring(message, dtype=np.uint16).reshape((height, width))
+        process(x, metadata)
+
+        #conn.send(res.astype(np.float32).tobytes())
+    	
+while True:
+    get_connection()
---
+++ dummy_send.py
@@ -0,0 +1,30 @@
+import socket
+import numpy as np
+import time
+
+host = socket.gethostname()    
+port = 9999             # The same port as used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+
+
+metadata = '{"probe":"fake_circle", "mm_per_pixel":".8"}'
+
+s.sendall(np.array([len(metadata)], dtype=np.int32).tobytes())
+s.sendall(bytes(metadata, "utf-8"))
+
+width = 200
+height = 300
+s.sendall(np.array([width, height], dtype=np.int32).tobytes())
+
+x, y = np.mgrid[-1:1:300j, -1:1:200j]
+
+r = np.sqrt(x**2 + y**2)
+print(r.shape)
+
+while(True):
+	for size in np.linspace(0, 1, 100):
+
+		s.sendall(((r < size) * 2**15).astype(np.uint16).tobytes())
+		time.sleep(.1)
+
